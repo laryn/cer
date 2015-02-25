a6b00b2e92dd1b06861c79c16d8f87e97df81428
Phéna Proxima <djphenaproxima@gmail.com>
2015-02-25 09:36:01 -0500

    Wrote passing tests of CER's basic CRUD functionality.

--- .install.sh
+++ .install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "Installing Drupal..."
-drush si -y -q --db-url=mysql://vlad:wibble@localhost/vladdb
-drush -y -q en admin_menu_toolbar cer devel entityreference features field_collection node_reference user_reference profile2 views_ui
+drush si -y -q --db-url=mysql://vlad:wibble@localhost/vladdb --account-pass=admin
+drush -y -q en admin_menu_toolbar cer cer_entity_settings cer_profile2 devel entityreference features field_collection node_reference user_reference profile2 simpletest views_ui
+drush -y -q en cer_tests
+
+# I. Hate. Overlay!
+drush -y -q dis overlay
+drush -y -q pmu-uninstall overlay
