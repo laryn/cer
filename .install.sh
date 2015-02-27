fde1973ec6fecda6f9c73f9ceb6a181d5a26221d
phenaproxima <djphenaproxima@gmail.com>
2015-02-27 13:23:34 -0500

    Moved tests into the main CER module, and fixed minor issues.

--- .install.sh
+++ .install.sh
@@ -3,8 +3,7 @@
 echo "Installing Drupal..."
 drush si -y -q --db-url=mysql://vlad:wibble@localhost/vladdb --account-pass=admin
 drush -y -q en admin_menu_toolbar cer cer_entity_settings cer_profile2 devel entityreference features field_collection node_reference user_reference profile2 simpletest views_ui
-drush -y -q en cer_tests
 
 # I. Hate. Overlay!
 drush -y -q dis overlay
-drush -y -q pmu-uninstall overlay
+drush -y -q pm-uninstall overlay
